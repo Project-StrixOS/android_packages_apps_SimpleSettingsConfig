fedaeb64766de4affb1d7787781b19487383b060
shuixx <shuixx@shuixx.com>
2026-01-30 04:22:12 +0000

    StrixOS

--- src/org/lineageos/settingsconfig/BootReceiver.java
+++ src/org/lineageos/settingsconfig/BootReceiver.java
@@ -23,7 +23,7 @@ import android.content.Intent;
 import android.provider.Settings;
 import android.util.Log;
 
-import lineageos.providers.LineageSettings;
+import strixos.providers.LineageSettings;
 
 import java.io.File;
 import java.io.IOException;
